7f6fcd0436aedc601a8986ed5d1648f3708816a4
peuldev <arfinpeul523@gmail.com>
2023-12-22 22:30:27 +0600

    load offers  data get id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,6 +23,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const specialOffers = client.db("Special_Offers").collection("Offers");
+    app.get("/offers", async (req, res) => {
+      const cursor = specialOffers.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/offers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await specialOffers.findOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
