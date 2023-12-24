cd11b91f4b87ac2e903687a9ff93ec1dae0b7abe
peuldev <arfinpeul523@gmail.com>
2023-12-24 19:48:02 +0600

    get room id

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await rooms.findOne(query);
+      res.send(result);
+    });
+
     app.get("/offers", async (req, res) => {
       const cursor = specialOffers.find();
       const result = await cursor.toArray();
